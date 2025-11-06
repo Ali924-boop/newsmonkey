302924a15fa3e46028536ed98fd26d9fdda570cb
Ali Raza <alirazasandha5@gmail.com>
2025-11-06 09:07:53 -0800

    Updated files and added components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const NewsItem = (props)=> {
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className="my-3">
+        <div className="card">
+          <div>
+        <span className="badge rounded-pill bg-danger">
+    {source}
+    <span className="visually-hidden">unread messages</span> 
+    </span>
+    </div>
+        <img src={!imageUrl? "https://cdn.arstechnica.net/wp-content/uploads/2025/04/2025_MOTOROLA_RAZR_FAMILY_KEY_VISUAL_HORIZONTAL-1536x1024.jpg ": imageUrl} 
+        className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">{title} </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text"><small className="text-muted">By {!author?"Unknown": author} on {new Date(date).toGMTString()}</small></p>
+          <a  rel='noreferrer' href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
+        </div>
+      </div> 
+      </div>
+    )
+  }
+
+
+export default NewsItem;
